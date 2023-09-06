912a6c2c66615bcc9bea04ae8fa9ad6919b52b69
Juanes Pinilla <juanpinpin@unisabana.edu.co>
2023-09-06 07:29:30 -0500

    First Commit

---
+++ src/main/java/com/inventario/donpedro/DonpedroApplication.java
@@ -0,0 +1,13 @@
+package com.inventario.donpedro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonpedroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonpedroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventario/donpedro/DonpedroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventario.donpedro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonpedroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
